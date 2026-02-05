7ed09cb78b1630845e272c2a9c83d6ed8aed68a8
Lucas dos Santos <104210335+kia2612@users.noreply.github.com>
2026-02-05 19:19:09 -0300

    Create ex113.py

---
+++ Python - mundo 3/ex113.py
@@ -0,0 +1,31 @@
+def leiaInt(msg):
+    while True:
+        try:
+            n = str(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mErro: por favor digite um número Inteiro!\033[m')
+            continue
+        except KeyboardInterrupt:
+            print("\033[31mEntrada interrompida pelo usuário.\033[m")
+            return 0
+        else:
+            return valor
+    
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = str(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mErro: por favor digite um número Real!\033[m')
+        except KeyboardInterrupt:
+            print("\033[31mEntrada interrompida pelo usuário.\033[m")
+            return 0
+        else:
+            return valor
+    
+def main():
+    i = leiaInt("Digite um número Inteiro: ")
+    f = leiaFloat("Digite um número Real: ")
+    print(f"O usuário digitou os números {i} e {f:.2f}")
